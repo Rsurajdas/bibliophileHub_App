a91057e50a0232dbcf7539e22ad2cc780e9de767
Rsurajdas <surajkumar18294@gmail.com>
2023-09-01 16:34:26 +0530

    book api documentation updated

--- routes/api/bookRoutes.js
+++ routes/api/bookRoutes.js
@@ -1,3 +1,36 @@
+import express from 'express';
+import {
+  getAllBooks,
+  createBook,
+  getBook,
+  updateBook,
+  deleteBook,
+  searchBook,
+  getAllBooksByGenre,
+  aggregationBook,
+} from '../../controllers/bookController';
+import { protect, restrictedTo } from '../../controllers/authController';
+import reviewRouter from './reviewRoutes';
+
+const bookRouter = express.Router();
+
+// Nested Routes
+bookRouter.use('/:bookId/reviews', reviewRouter);
+bookRouter
+  .route('/')
+  .get(getAllBooks)
+  .post(protect, restrictedTo('author', 'admin'), createBook);
+bookRouter.get('/search', searchBook);
+bookRouter
+  .route('/:id')
+  .get(protect, getBook)
+  .patch(protect, restrictedTo('author', 'admin'), updateBook)
+  .delete(protect, restrictedTo('author', 'admin'), deleteBook);
+bookRouter.get('/get-books/:genreId', getAllBooksByGenre);
+bookRouter.get('/genres/grouped-by-genres', aggregationBook);
+
+export default bookRouter;
+
 /**
  * @swagger
  * components:
@@ -74,39 +107,6 @@
  *             url:
  *              type: string
  */
-
-import express from 'express';
-import {
-  getAllBooks,
-  createBook,
-  getBook,
-  updateBook,
-  deleteBook,
-  searchBook,
-  getAllBooksByGenre,
-  aggregationBook,
-} from '../../controllers/bookController';
-import { protect, restrictedTo } from '../../controllers/authController';
-import reviewRouter from './reviewRoutes';
-
-const bookRouter = express.Router();
-
-// Nested Routes
-bookRouter.use('/:bookId/reviews', reviewRouter);
-bookRouter
-  .route('/')
-  .get(getAllBooks)
-  .post(protect, restrictedTo('author', 'admin'), createBook);
-bookRouter.get('/search', searchBook);
-bookRouter
-  .route('/:id')
-  .get(protect, getBook)
-  .patch(protect, restrictedTo('author', 'admin'), updateBook)
-  .delete(protect, restrictedTo('author', 'admin'), deleteBook);
-bookRouter.get('/get-books/:genreId', getAllBooksByGenre);
-bookRouter.get('/genres/grouped-by-genres', aggregationBook);
-
-export default bookRouter;
 
 /**
  * @swagger
