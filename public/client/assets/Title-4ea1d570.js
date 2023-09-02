1b617f5ab7c970778f1ffa71e9dfe2d09b4d4637
Rsurajdas <surajkumar18294@gmail.com>
2023-09-02 14:32:24 +0530

    login bug fix

--- public/client/assets/Title-4ea1d570.js
+++
@@ -1,1 +0,0 @@
-import{P as t,j as e}from"./index-626fd1d9.js";const s=({element:r})=>e.jsx(e.Fragment,{children:r});s.propTypes={element:t.element};export{s as T};
---
+++ public/client/assets/Title-5e271a8f.js
@@ -0,0 +1,1 @@
+import{P as t,j as e}from"./index-7143f6c6.js";const s=({element:r})=>e.jsx(e.Fragment,{children:r});s.propTypes={element:t.element};export{s as T};
